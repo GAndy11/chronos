3cb7de1630f3be5b204d975e1acfcc4b4e6e77cc
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2020-09-29 16:25:12 -0500

    Vistas iniciales para tiempos y usuarios

--- controllers/tiemposController.php
+++ controllers/tiemposController.php
@@ -2,9 +2,10 @@
 
 class tiemposController
 {
-    public function Listar(string $men = "test")
+    public function Listar(string $men = "")
     {
         $mensaje = $men;
+        require_once "./views/layouts/nav.php";
         require_once "./views/tiempos/listar.php";
     }
 }
